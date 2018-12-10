9c4d0da7308410ca18127a014d111560e07e6593
snowxu17 <snowx1997@gmail.com>
2018-12-10 13:36:11 -0600

    Update page

--- indexSketch.js
+++ indexSketch.js
@@ -59,7 +59,16 @@ function setup() {
     // gbutton.parent("buttons");
 }
 
-function draw() {
+var i = 0;
+var txt = 'Lorem ipsum typing effect!'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
 }
 
 
